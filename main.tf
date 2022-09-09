8975184c81aa8040c4c6e0a3a0eb935b741e1e5b
Tobechi Bob-Duru <bobduru@gmail.com>
2022-09-08 22:29:26 -0400

    added Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 terraform {
-  required_version = "~>1.1.7"
+  required_version = "~>1.1"
 
   backend "s3" {
     bucket         = "kojitech.3.tier.arch.jnj" # 
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 locals {
-  vpc_id             = module.vpc.vpc_id 
+  vpc_id             = module.vpc.vpc_id
   pub_subnet_id      = module.vpc.pub_subnetid
   private_subnet_id  = module.vpc.private_subnetid
   database_subnet_id = module.vpc.database_subnetid
@@ -32,10 +32,10 @@ module "vpc" {
   source = "git::https://github.com/Charles123Bob-Duru/kojitechs-vpc-module.git?ref=v1.1.0"
 
   vpc_tags = {
-    Name = "Kojitechs_vpc"
+    Name = "Kojitechs_vpc_${terraform.workspace}"
   }
-  vpc_cidr      = "10.0.0.0/16"
-  public_cidr   = ["10.0.0.0/24", "10.0.2.0/24", "10.0.4.0/24"]
-  private_cidr  = ["10.0.1.0/24", "10.0.3.0/24"]
-  database_cidr = ["10.0.5.0/24", "10.0.7.0/24"]
+  vpc_cidr      = var.vpc_cidr
+  public_cidr   = var.public_cidr
+  private_cidr  = var.private_cidr
+  database_cidr = var.database_cidr
 }
